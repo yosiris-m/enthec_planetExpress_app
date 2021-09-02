8057d671197a627e32b8b854fb18abc7814db8e4
Yosiris Mariñez <yosiris0510@gmail.com>
2021-09-02 22:24:56 +0200

    added react router && sass styles

--- planet_expres_app/src/App.js
+++ planet_expres_app/src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+import "./App.js";
+
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <div>hello</div>
-    </>
+    </Router>
   );
 }
 
